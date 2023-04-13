16e26c9e1267b30b740ec30bf319a794e2b5d021
LeoLChalot <larouchalotleo@gmail.com>
2023-04-13 08:51:54 +0200

    DISPLAY | Modif Footer

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,7 +5,7 @@ let btnBurgerSpan = document.querySelectorAll("#btn-burger span");
 let copyright = document.querySelector(".copyright");
 let body = document.querySelector("body");
 
-console.log(btnBurger);
+// console.log(btnBurger);
 
 btnBurger.addEventListener("click", () => {
 	btnBurger.classList.toggle("active");
@@ -37,48 +37,49 @@ btnBurger.addEventListener("click", () => {
 // 	this.isDeleting = false;
 // };
 
-let TxtType = function (el, toRotate, period) {
-	this.toRotate = toRotate;
-	this.el = el;
-	this.loopNum = 0;
-	this.period = parseInt(period, 10) || 2000;
-	this.txt = "";
-	this.tick();
-	this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function () {
-	let i = this.loopNum % this.toRotate.length;
-	let fullTxt = this.toRotate[i];
-
-	if (this.isDeleting) {
-		this.txt = fullTxt.substring(0, this.txt.length - 1);
-	} else {
-		this.txt = fullTxt.substring(0, this.txt.length + 1);
-	}
-
-	this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-
-	let that = this;
-	let delta = 200 - Math.random() * 100;
-
-	if (this.isDeleting) {
-		delta /= 2;
-	}
-
-	if (!this.isDeleting && this.txt === fullTxt) {
-		delta = this.period;
-		this.isDeleting = true;
-	} else if (this.isDeleting && this.txt === "") {
+class TxtType {
+	constructor(el, toRotate, period) {
+		this.toRotate = toRotate;
+		this.el = el;
+		this.loopNum = 0;
+		this.period = parseInt(period, 10) || 2000;
+		this.txt = "";
+		this.tick();
 		this.isDeleting = false;
-		this.loopNum++;
-		delta = 500;
 	}
+	tick() {
+		let i = this.loopNum % this.toRotate.length;
+		let fullTxt = this.toRotate[i];
 
-	setTimeout(function () {
-		that.tick();
-	}, delta);
-};
+		if (this.isDeleting) {
+			this.txt = fullTxt.substring(0, this.txt.length - 1);
+		} else {
+			this.txt = fullTxt.substring(0, this.txt.length + 1);
+		}
+
+		this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+		let that = this;
+		let delta = 200 - Math.random() * 100;
+
+		if (this.isDeleting) {
+			delta /= 2;
+		}
+
+		if (!this.isDeleting && this.txt === fullTxt) {
+			delta = this.period;
+			this.isDeleting = true;
+		} else if (this.isDeleting && this.txt === "") {
+			this.isDeleting = false;
+			this.loopNum++;
+			delta = 500;
+		}
+
+		setTimeout(function () {
+			that.tick();
+		}, delta);
+	}
+}
 
 window.onload = function () {
 	let elements = document.getElementsByClassName("typewrite");
@@ -95,3 +96,28 @@ window.onload = function () {
 	css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
 	document.body.appendChild(css);
 };
+
+// ? Flux RSS
+// ! RS policy: No 'Access-Control-Allow-Origin' header i
+// let url = "https://www.technologyreview.com/feed/";
+// let url = "https://blogs.microsoft.com/AI/feed/";
+// let url = "https://research.aimultiple.com/feed/";
+// let url = "https://towardsdatascience.com/feed"
+// let url = "https://www.marktechpost.com/category/technology/artificial-intelligence/feed/";
+
+// * data format XML
+let url = "https://blog.google/technology/ai/rss/";
+
+
+
+try {
+	axios
+	.get(url)
+	.then((res) => {
+		let data = res.data
+		console.log(data);
+	})
+} catch {
+	(err) => console.log(err);
+}
+
