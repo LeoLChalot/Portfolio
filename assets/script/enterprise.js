d62901a85c2812dc18a5ebc180155ddf771370d1
leolchalot <larouchalotleo@gmail.com>
2023-06-27 23:29:12 +0200

    nav entreprise OK

--- assets/script/enterprise.js
+++ assets/script/enterprise.js
@@ -1,53 +1,66 @@
 $(document).ready(function () {
-  var curPage = 1;
-  var numOfPages = $(".skw-page").length;
-  var animTime = 1000;
-  var scrolling = false;
-  var pgPrefix = ".skw-page-";
+  var largeur = window.innerWidth;
 
-  function pagination() {
-    scrolling = true;
+  console.log(largeur);
 
-    $(pgPrefix + curPage)
-      .removeClass("inactive")
-      .addClass("active");
-    $(pgPrefix + (curPage - 1)).addClass("inactive");
-    $(pgPrefix + (curPage + 1)).removeClass("active");
+  if (largeur > 1023) {
+    var curPage = 1;
+    var numOfPages = $(".skw-page").length;
+    var animTime = 1000;
+    var scrolling = false;
+    var pgPrefix = ".skw-page-";
 
-    setTimeout(function () {
-      scrolling = false;
-    }, animTime);
-  }
+    function pagination() {
+      scrolling = true;
 
-  function navigateUp() {
-    if (curPage === 1) return;
-    curPage--;
-    pagination();
-  }
+      $(pgPrefix + curPage)
+        .removeClass("inactive")
+        .addClass("active");
+      $(pgPrefix + (curPage - 1)).addClass("inactive");
+      $(pgPrefix + (curPage + 1)).removeClass("active");
 
-  function navigateDown() {
-    if (curPage === numOfPages) return;
-    curPage++;
-    pagination();
-  }
-
-  $(document).on("mousewheel DOMMouseScroll", function (e) {
-    if (scrolling) return;
-    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
-      navigateUp();
-    } else {
-      navigateDown();
+      setTimeout(function () {
+        scrolling = false;
+      }, animTime);
     }
-  });
 
-  $(document).on("keydown", function (e) {
-    if (scrolling) return;
-    if (e.which === 38) {
-      navigateUp();
-    } else if (e.which === 40) {
-      navigateDown();
+    function navigateUp() {
+      if (curPage === 1) return;
+      curPage--;
+      pagination();
     }
-  });
+
+    function navigateDown() {
+      if (curPage === numOfPages) return;
+      curPage++;
+      pagination();
+    }
+
+    $(document).on("mousewheel DOMMouseScroll", function (e) {
+      if (scrolling) return;
+      if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+        navigateUp();
+      } else {
+        navigateDown();
+      }
+    });
+
+    $(document).on("keydown", function (e) {
+      if (scrolling) return;
+      if (e.which === 38) {
+        navigateUp();
+      } else if (e.which === 40) {
+        navigateDown();
+      }
+    });
+  } else {
+    let pagesStatus = $(".skw-page");
+    console.log(pagesStatus);
+    for (let i = 0; i < pagesStatus.length; i++) {
+      console.log(pagesStatus[i]);
+      pagesStatus.removeClass("active");
+    }
+  }
 
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.querySelectorAll("nav a");
